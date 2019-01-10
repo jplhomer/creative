ff3768a396632cfb4ee0ca790351bc20e2b64a7e
Josh Larson <jplhomer@gmail.com>
2019-01-09 22:17:59 -0600

    Well this is dumb

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,8 +41,7 @@ module.exports = {
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-          `gatsby-remark-oembed`
+          `gatsby-remark-smartypants`
         ],
       },
     },
