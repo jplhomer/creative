876c63dc998e770f27e122b21e947c73d231896a
Josh Larson <jplhomer@gmail.com>
2019-01-10 21:24:21 -0600

    Add some sexy styles and Dan Abramov copies

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
             },
           },
           `gatsby-remark-prismjs`,
+          `gatsby-remark-autolink-headers`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
           `gatsby-embed-tweets`
